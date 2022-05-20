04f8e696a6036bf3db4d1b8a6ccf09ccb06eaaf4
bradleycarloss <101831484+bradleycarloss@users.noreply.github.com>
2022-05-20 15:55:41 -0400

    Initial Commit
    Brad getting shit done

---
+++ Soulspin-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Soulspin-Bridging-Header.h
+//  Soulspin
+//
+//  Created by Bradley Johnson on 5/20/22.
+//
+
+#ifndef Soulspin_Bridging_Header_h
+#define Soulspin_Bridging_Header_h
+
+#import <SpotifyiOS/SpotifyiOS.h>
+
+#endif /* Soulspin_Bridging_Header_h */
---
+++ Soulspin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Soulspin
+//
+//  Created by Bradley Johnson on 5/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
